53bb175eb7bb05d173aa63555b87d8cdd8a9e6cc
Bill Haofei Gong <xmagic5589@gmail.com>
2015-05-02 14:06:25 +1000

    Added an alert image when battery is drained (<10% and not charging)

--- kindle/update-weather.sh
+++ kindle/update-weather.sh
@@ -5,22 +5,28 @@
 # 5,20,35,50 * * * * /bin/sh /mnt/base-us/kindle-weather-display/update-weather.sh > /dev/null 2>&1
 # BTW, I choose to put all files on USB drive instead of system partition. Also the update is shorten to every 15 minutes with offset of 5 minutes earlier than server updates.
 
-# Stop services to turn Kindle into a display puppet :P
-/etc/init.d/framework stop
-/etc/init.d/powerd stop
-
 cd "$(dirname "$0")"
-
-# Remove old file
-rm weather-update.png
 # Clear the display
 eips -c
 eips -c
 
-# Download updated weather picture
-if wget "http://Your-Website-Domain/weather-update.png"; then
-	eips -g weather-update.png
+# Check battery capacity
+BATTERY=`grep -o "[0-9]*" /sys/devices/system/yoshi_battery/yoshi_battery0/battery_capacity`
+CURRENT=`cat /sys/devices/system/yoshi_battery/yoshi_battery0/battery_current`
+
+if [ $BATTERY -le 10 ] && [ $CURRENT -le 0 ]; then
+	# Show battery drained image
+    eips -g battery-drained.png
 else
-	# Here comes the sad face
-	eips -g weather-error.png
+	# Stop services to turn Kindle into a display puppet :P
+	/etc/init.d/framework stop
+	/etc/init.d/powerd stop
+	# Remove old file
+	rm weather-update.png
+	# Download updated weather picture
+	if wget "http://Your-Website-Domain/weather-update.png"; then
+		eips -g weather-update.png
+	else
+		eips -g weather-error.png
+	fi
 fi
